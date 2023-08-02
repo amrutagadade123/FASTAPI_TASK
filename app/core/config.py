ee6bd22ab5a787c35c30582717403bf5d2ee2140
amrutagadade123 <amruta.gadade@sumasoft.net>
2023-08-02 17:32:10 +0530

    Part10 JWT

---
+++ app/core/config.py
@@ -0,0 +1,33 @@
+import pathlib
+
+from pydantic import AnyHttpUrl, BaseSettings, EmailStr, validator
+from typing import List, Optional, Union
+
+ROOT = pathlib.Path(__file__).resolve().parent.parent
+
+class Settings(BaseSettings):
+    API_V1_STR: str = "/api/v1"
+    JWT_SECRET: str = "TEST_SECRET_DO_NOT_USE_IN_PROD"
+    ALGORITHM: str = "HS256"
+
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
+
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
+
+    @validator("BACKEND_CORS_ORIGINS", pre=True)
+    def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
+        if isinstance(v, str) and not v.startswith("["):
+            return [i.strip() for i in v.split(",")]
+        elif isinstance(v, (list, str)):
+            return v
+        raise ValueError(v)
+
+    SQLALCHEMY_DATABASE_URI: Optional[str] = "sqlite:///example.db"
+    FIRST_SUPERUSER: EmailStr = "admin@recipeapi.com"
+    FIRST_SUPERUSER_PW: str = "CHANGEME"
+
+    class Config:
+        case_sensitive = True
+
+
+settings = Settings()
---
+++ app/db/init_db.py
@@ -0,0 +1,66 @@
+import logging
+from sqlalchemy.orm import Session
+
+from app import crud, schemas
+from app.db import base  # noqa: F401
+from app.core.config import settings
+
+logger = logging.getLogger(__name__)
+
+
+RECIPES = [
+    {
+        "id": 1,
+        "label": "Chicken Vesuvio",
+        "source": "Serious Eats",
+        "url": "http://www.seriouseats.com/recipes/2011/12/chicken-vesuvio-recipe.html",
+    },
+    {
+        "id": 2,
+        "label": "Chicken Paprikash",
+        "source": "No Recipes",
+        "url": "http://norecipes.com/recipe/chicken-paprikash/",
+    },
+    {
+        "id": 3,
+        "label": "Cauliflower and Tofu Curry Recipe",
+        "source": "Serious Eats",
+        "url": "http://www.seriouseats.com/recipes/2011/02/cauliflower-and-tofu-curry-recipe.html",  # noqa
+    },
+]
+
+
+
+
+def init_db(db: Session) -> None:
+  
+    if settings.FIRST_SUPERUSER:
+        user = crud.user.get_by_email(db, email=settings.FIRST_SUPERUSER)
+        if not user:
+            user_in = schemas.UserCreate(
+                full_name="Initial Super User",
+                email=settings.FIRST_SUPERUSER,
+                is_superuser=True,
+                password=settings.FIRST_SUPERUSER_PW,
+            )
+            user = crud.user.create(db, obj_in=user_in)  # noqa: F841
+        else:
+            logger.warning(
+                "Skipping creating superuser. User with email "
+                f"{settings.FIRST_SUPERUSER} already exists. "
+            )
+        if not user.recipes:
+            for recipe in RECIPES:
+                recipe_in = schemas.RecipeCreate(
+                    label=recipe["label"],
+                    source=recipe["source"],
+                    url=recipe["url"],
+                    submitter_id=user.id,
+                )
+                crud.recipe.create(db, obj_in=recipe_in)
+    else:
+        logger.warning(
+            "Skipping creating superuser.  FIRST_SUPERUSER needs to be "
+            "provided as an env variable. "
+            "e.g.  FIRST_SUPERUSER=admin@api.coursemaker.io"
+        )
